89c1b5922eac16c0ae4fbd1a7deea9d8e8cd2b5f
peerawattae <peerawat.thee@ku.th>
2026-08-31 20:28:18 +0700

    add feature login by oauth for user in backend

---
+++ app/api/auth/google/login/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const googleAuthUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+
+    googleAuthUrl.searchParams.set('client_id', process.env.GOOGLE_CLIENT_ID);
+    googleAuthUrl.searchParams.set('redirect_uri', process.env.NEXT_PUBLIC_REDIRECT_URI);
+    googleAuthUrl.searchParams.set('response_type', 'code');
+    googleAuthUrl.searchParams.set('scope', 'openid email profile');
+    googleAuthUrl.searchParams.set('access_type', 'offline');
+    googleAuthUrl.searchParams.set('prompt', 'consent');
+
+    return NextResponse.json({
+        status: 'success',
+        authorization_url: googleAuthUrl.toString(),
+    });
+}
